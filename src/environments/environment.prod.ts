2700989143651d05f78690c12940a4d9a0a16e7b
j c <jianchang83@gmail.com>
2019-10-14 01:07:51 -0600

    maybe

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiamM3ODM5MCIsImEiOiJjazFwbHZ6NHUweWZoM25wazd2dnk2NjdmIn0.WhKYdt7vlnfwXYZOKix4PQ'
+  }
+}
